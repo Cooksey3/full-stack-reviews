0e30b838867aecc1aa30d2a4f5cf00aa2c08f8a5
cooksey3 <chris@cookseys.org>
2018-03-02 10:07:25 -0500

    Initial commit

---
+++ src/main/java/org/wecancodeit/columbus/fullstackreviews/FullStackReviewsApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.columbus.fullstackreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackReviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackReviewsApplication.class, args);
+	}
+}
---
+++ src/test/java/org/wecancodeit/columbus/fullstackreviews/FullStackReviewsApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wecancodeit.columbus.fullstackreviews;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FullStackReviewsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
