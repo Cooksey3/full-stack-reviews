8b368d71d30b0363c6d7106b5208ed8c151c9a95
cooksey3 <chris@cookseys.org>
2018-03-05 22:06:56 -0500

    Finished styling book page

--- src/main/java/org/wecancodeit/columbus/fullstackreviews/BookReviewsPopulator.java
+++ src/main/java/org/wecancodeit/columbus/fullstackreviews/BookReviewsPopulator.java
@@ -26,10 +26,11 @@ public class BookReviewsPopulator implements CommandLineRunner {
 		Tag thriller = tagRepo.save(new Tag("Thriller"));
 		Tag action = tagRepo.save(new Tag("Action"));
 		Tag fantasy = tagRepo.save(new Tag("Fantasy"));
-		bookRepo.save(new Book("The Lord of the Rings", "/images/lordoftherings.png", "A thrilling fantasy trilogy filled with peril and sacrifice",
+		bookRepo.save(new Book("The Lord of the Rings", "/images/lordoftherings.png",
+				"A thrilling fantasy trilogy filled with peril and sacrifice, featuring a well-ordered fantasy world. Tolkien's finest work.",
 				fiction, action, fantasy, adventure));
-		bookRepo.save(new Book("Marine Sniper: 93 Confirmed Kills",
-				"The true story of America's most successful sniper", nonFiction, action, thriller));
+		bookRepo.save(new Book("Marine Sniper: 93 Confirmed Kills", "/images/marinesniper.png",
+				"The true story of America's deadliest sniper during the Vietnam War", nonFiction, action, thriller));
 	}
 
 }
